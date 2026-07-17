d080209bfb3957eec93435471aab8b06719c265f
tafasad <tafasad@users.noreply.github.com>
2026-07-17 11:52:11 -0300

    feat: android location photos audio mqtt, permissions, media support

--- android/app/src/main/java/com/nexus/app/MainActivity.java
+++ android/app/src/main/java/com/nexus/app/MainActivity.java
@@ -2,24 +2,30 @@ package com.nexus.app;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.webkit.WebResourceRequest;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
 import com.getcapacitor.BridgeActivity;
 
 public class MainActivity extends BridgeActivity {
   @Override
-  public void onStart() {
-    super.onStart();
+  public void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
     WebView webView = this.getBridge().getWebView();
-    webView.getSettings().setAllowFileAccess(true);
-    webView.getSettings().setAllowContentAccess(true);
+    WebSettings s = webView.getSettings();
+    s.setJavaScriptEnabled(true);
+    s.setMediaPlaybackRequiresUserGesture(false);
+    s.setAllowFileAccess(true);
+    s.setAllowContentAccess(true);
+    s.setGeolocationEnabled(true);
     webView.setWebViewClient(new WebViewClient() {
       @Override
       public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
         Uri uri = request.getUrl();
         String host = uri.getHost();
-        if (host != null && (host.equals("192.168.18.14") || host.equals("localhost") || host.equals("127.0.0.1") || host.endsWith(".loca.lt"))) {
+        if (host != null && (host.equals("192.168.18.14") || host.equals("localhost") || host.equals("127.0.0.1") || host.endsWith(".loca.lt") || host.endsWith("trycloudflare.com") || host.contains("broker.hivemq.com"))) {
           view.loadUrl(uri.toString());
           return true;
         }
