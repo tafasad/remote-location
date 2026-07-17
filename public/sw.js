ccc50026b0c9841468ea9b500aedb344ffef1b4f
tafasad <tafasad@users.noreply.github.com>
2026-07-16 23:33:31 -0300

    feat: installable PWA with offline cache and app icons

---
+++ public/sw.js
@@ -0,0 +1,8 @@
+self.addEventListener('install', (e) => self.skipWaiting());
+self.addEventListener('activate', (e) => e.waitUntil(self.clients.claim()));
+self.addEventListener('fetch', (e) => {
+  if (e.request.method !== 'GET') return;
+  e.respondWith(
+    fetch(e.request).then((r) => r).catch(() => caches.match('/'))
+  );
+});
